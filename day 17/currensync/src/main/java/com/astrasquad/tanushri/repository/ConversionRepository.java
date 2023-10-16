24caf76b6836c25b62ef14a4279dd7606cdc9514
ADTANU <104961756+ADTANU@users.noreply.github.com>
2023-10-16 17:38:49 +0530

    Add files via upload

---
+++ day 17/currensync/src/main/java/com/astrasquad/tanushri/repository/ConversionRepository.java
@@ -0,0 +1,12 @@
+package com.astrasquad.tanushri.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.astrasquad.tanushri.model.Conversion;
+
+
+
+public interface ConversionRepository extends JpaRepository<Conversion,Long> {
+
+}
---
+++ day 17/currensync/src/main/java/com/astrasquad/tanushri/service/ConversionService.java
@@ -0,0 +1,38 @@
+package com.astrasquad.tanushri.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.astrasquad.tanushri.dto.ConversionRequest;
+import com.astrasquad.tanushri.dto.ConversionResponse;
+
+@Service
+public class ConversionService {
+
+	public ConversionResponse createConversion(ConversionRequest conversionRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ConversionResponse getConversionById(Long cid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List<ConversionResponse> getAllConversions() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public ConversionResponse updateConversion(Long cid, ConversionRequest conversionRequest) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteConversion(Long cid) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
